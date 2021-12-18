333ed239a99fa56e740202928f3ed5b0102ef0b7
Christian Rebischke <chris@shibumi.dev>
2021-12-18 14:59:08 +0100

    fix: implement missing Types: cran, conan, swift
    This commit implements the missing purl Types cran,
    conan and swift. Furthermore, it adds a function
    for validating custom Rules for these types.

--- packageurl.go
+++ packageurl.go
@@ -53,6 +53,10 @@ var (
 var (
 	// TypeBitbucket is a pkg:bitbucket purl.
 	TypeBitbucket = "bitbucket"
+	// TypeCran is a pkg:cran purl.
+	TypeCran = "cran"
+	// TypeConan is pkg:conan purl.
+	TypeConan = "conan"
 	// TypeComposer is a pkg:composer purl.
 	TypeComposer = "composer"
 	// TypeDebian is a pkg:deb purl.
@@ -77,6 +81,8 @@ var (
 	TypePyPi = "pypi"
 	// TypeRPM is a pkg:rpm purl.
 	TypeRPM = "rpm"
+	// TypeSwift is pkg:swift purl
+	TypeSwift = "swift"
 )
 
 // Qualifier represents a single key=value qualifier in the package url
@@ -329,6 +335,10 @@ func FromString(purl string) (PackageURL, error) {
 		return PackageURL{}, errors.New("name is required")
 	}
 
+	if err := validateCustomRules(purlType, name, namespace, version, qualifiers); err != nil {
+		return PackageURL{}, err
+	}
+
 	return PackageURL{
 		Type:       purlType,
 		Namespace:  namespace,
@@ -366,4 +376,43 @@ func typeAdjustName(purlType, name string) string {
 		return strings.ToLower(strings.ReplaceAll(name, "_", "-"))
 	}
 	return name
+}
+
+func validQualifierKey(key string) bool {
+	return QualifierKeyPattern.MatchString(key)
+}
+
+func validateCustomRules(purlType, name, ns, version string, qualifiers Qualifiers) error {
+	q := qualifiers.Map()
+	switch purlType {
+	case TypeConan:
+		if ns != "" {
+			if val, ok := q["channel"]; ok {
+				if val == "" {
+					return errors.New("the qualifier channel must be not empty if namespace is present")
+				}
+			} else {
+				return errors.New("channel qualifier does not exist")
+			}
+		}
+		if ns == "" {
+			if val, ok := q["channel"]; ok {
+				if val != "" {
+					return errors.New("namespace is required if channel is non empty")
+				}
+			}
+		}
+	case TypeSwift:
+		if ns == "" {
+			return errors.New("namespace is required")
+		}
+		if version == "" {
+			return errors.New("version is required")
+		}
+	case TypeCran:
+		if version == "" {
+			return errors.New("version is required")
+		}
+	}
+	return nil
 }
